21a90851ef45ff28f2d856884796b2f525b57952
Prab <prab.missier@gmail.com>
2021-11-04 16:36:58 +1100

    Added solution for Borze

---
+++ Borze/Source.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	string inp;
+	cin >> inp;
+
+	string op;
+	for (int i = 0; i < inp.length(); ++i)
+	{
+		if (inp[i] == '.')
+			op += '0';
+		else if (inp[i] == '-' && inp[i + 1] == '.') {
+			op += '1';
+			i++;
+		}
+		else if (inp[i] == '-' && inp[i + 1] == '-') {
+			op += '2';
+			i++;
+		}
+	}
+
+	cout << op << endl;
+	return 0;
+}
